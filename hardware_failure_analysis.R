a19a313ed030a18d74b0f98c1d9829b2cadb687a
chicsof <sophia.97@windowslive.com>
2018-10-29 19:57:50 +0000

    Added column for unresponsiveness boolean

--- hardware_failure_analysis.R
+++ hardware_failure_analysis.R
@@ -1,8 +1,9 @@
+
 getwd()
 setwd("/n_mon")
 
 #import merged data set
-cpuDataSet <- read.table("cpu.csv",fill = TRUE,header=TRUE, sep=",", stringsAsFactors=FALSE)
+cpuDataSet <- read.table("cpu.csv",fill = TRUE,header=TRUE, sep=",",  stringsAsFactors=FALSE)
 
 head(cpuDataSet)
 
@@ -19,7 +20,7 @@ cpuDataSet$CPU. <- NULL
 cpuDataSet$CPUs <- NULL
 
 #avarege time to 15 mins to make data for each day uniform
-cpuDataSet$timestamp <- as.POSIXlt(round(as.double(cpuDataSet$timestamp)/(15*60))*(15*60),origin=(as.POSIXlt('1970-01-01')))
+cpuDataSet$timestamp <-as.POSIXlt(round(as.double(cpuDataSet$timestamp)/(15*60))*(15*60),origin=(as.POSIXlt('1970-01-01')))
 
 
 #create date and time as seperate fileds for the next step
@@ -37,3 +38,14 @@ max(frTable)
 plot(cpuDataSet$timestamp, cpuDataSet$totalCPUUsage)
 #threshold for desired range
 abline(h=75, col="red")
+
+#return all points above threshold
+busyTimes <- subset(cpuDataSet, cpuDataSet$totalCPUUsage >= 75);
+
+#get a range
+max(busyTimes$Time)
+min(busyTimes$Time)
+
+#get an avarage
+library(chron)
+mean(times(busyTimes$Time))
